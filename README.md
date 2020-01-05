6747cbe8bb81b44f8e493e9ba2a906e095477ec9
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2020-01-05 17:30:35 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Goals 
+- Scrape all files from ttu crime log site with .pdf extentions 
+- Extract data from pdf files
+- Visulise campus crime data 
+## Sources 
+- [Working with pdf files in python GFG](https://www.geeksforgeeks.org/working-with-pdf-files-in-python/)
+- [How to crack a pdf password](https://ourcodeworld.com/articles/read/939/how-to-crack-a-pdf-password-with-brute-force-using-john-the-ripper-in-kali-linux)
+
---
+++ extract_data.py
@@ -0,0 +1,27 @@
+# importing required modules 
+import PyPDF2 
+import pikepdf
+
+
+for x in 
+pdf = pikepdf.open('121819_main.pdf')
+pdf.save('extractable.pdf')
+  
+# creating a pdf file object 
+pdfFileObj = open('extractable.pdf', 'rb') 
+  
+# creating a pdf reader object 
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+  
+# printing number of pages in pdf file 
+print(pdfReader.numPages) 
+  
+# creating a page object 
+pageObj = pdfReader.getPage(0) 
+  
+# extracting text from page 
+print(pageObj.extractText()) 
+  
+# closing the pdf file object 
+pdfFileObj.close() 
+
