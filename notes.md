57892804311046af199fbc8ba9aee16d6b1bb672
Joseph Martinez <39905973+NotJoeMartinez@users.noreply.github.com>
2020-02-01 17:05:37 -0600

    read.py now prints out the contentce of every pdf file in decrypted_pdfs.. I think

---
+++ notes.md
@@ -0,0 +1,2 @@
+# 2/01/20
+- pikepdf does not currently implement text extraction. 
--- read.py
+++ read.py
@@ -1,2 +1,16 @@
 # read the stuff in decryped_pdfs 
+import os 
+import pprint
+import PyPDF2 
+import pikepdf
+
+path = 'decrypted_pdfs/'
+# this goes through the decrypted_pdf dir and prints out the text of each pdf
+for filename in os.listdir("decrypted_pdfs"):
+    pdfFileObj = open(path + filename, 'rb') 
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    pageObj = pdfReader.getPage(0) 
+    print(pageObj.extractText()) 
+
+
 
