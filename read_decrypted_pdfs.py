c64f93ca08c347e80536ed01a87cf2ee422122b8
joe martinez <notjoemartinez@protonmail.com>
2021-01-31 21:23:23 -0600

    decrypted pdfs, extracted text from decrypted pdfs

--- read_decrypted_pdfs.py
+++ read_decrypted_pdfs.py
@@ -4,13 +4,36 @@ import pprint
 import PyPDF2 
 import pikepdf
 
-path = 'decrypted_pdfs/'
-# this goes through the decrypted_pdf dir and prints out the text of each pdf
-for filename in os.listdir("decrypted_pdfs"):
-    pdfFileObj = open(path + filename, 'rb') 
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    pageObj = pdfReader.getPage(0) 
-    print(pageObj.extractText()) 
+  
+def decrypt_pdfs(path, decryped_path):
+    for filename in os.listdir(path):
+        
+        # decrypt pdf
+        pdf = pikepdf.open(path+filename)
+        pdf.save(decryped_path+filename)
 
-# TO DO: Save extracted text to a text file 
+        # read decrypted pdf
+        pdfFileObj = open(decryped_path+filename, 'rb') 
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+
+def read_pdfs(decrypted_path):
+    # this goes through the decrypted_pdf dir and prints out the text of each pdf
+    for filename in os.listdir(decrypted_path):
+        pdfFileObj = open(decrypted_path + filename, 'rb') 
+        pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+        pageObj = pdfReader.getPage(0) 
+        pdf_text = pageObj.extractText()
+        with open("pdf_text/" + filename[:-3] + 'txt', 'w') as writer:
+            writer.write(pdf_text)
+
+
+paths = ['pdfs/2016/', 'pdfs/2017/', 'pdfs/2018/', 'pdfs/2019/', 'pdfs/2020/']
+
+decrypted_path = 'decrypted_pdfs/'
+
+# for path in paths:
+#     decrypt_pdfs(path, decrypted_path)
+
+read_pdfs(decrypted_path)
+
 
