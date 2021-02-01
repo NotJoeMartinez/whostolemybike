9718367a42a0db3d4318fdf83d2cb0c62789a380
joe martinez <notjoemartinez@protonmail.com>
2021-01-31 19:58:32 -0600

    scraped ..some pdfs

--- scrape_ttu_crime_logs.py
+++ scrape_ttu_crime_logs.py
@@ -3,18 +3,27 @@ import requests
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
-url = "https://www.depts.ttu.edu/ttpd/daily/2020/TTU_Main/index.php"
-
-#If there is no such folder, the script will create one automatically
-folder_location = r'pdfs'
-if not os.path.exists(folder_location):os.mkdir(folder_location)
-
-response = requests.get(url)
-soup= BeautifulSoup(response.text, "html.parser")     
+urls = [
+    'https://www.depts.ttu.edu/ttpd/daily/2017/TTU_Main/index.php',
+    'https://www.depts.ttu.edu/ttpd/daily/2018/TTU_Main/index.php',
+    'https://www.depts.ttu.edu/ttpd/daily/2019/TTU_Main/index.php',
+    'https://www.depts.ttu.edu/ttpd/daily/2020/TTU_Main/index.php',
+    ]
 
 
-for link in soup.select("a[href$='.pdf']"):
-    #Name the pdf files using the last portion of each link which are unique in this case
-    filename = os.path.join(folder_location,link['href'].split('/')[-1])
-    with open(filename, 'wb') as f:
-        f.write(requests.get(urljoin(url,link['href'])).content)
+for x in urls:
+    url = x 
+
+    #If there is no such folder, the script will create one automatically
+    folder_location = r'pdfs'
+    if not os.path.exists(folder_location):os.mkdir(folder_location)
+
+    response = requests.get(url)
+    soup= BeautifulSoup(response.text, "html.parser")     
+
+
+    for link in soup.select("a[href$='.pdf']"):
+        #Name the pdf files using the last portion of each link which are unique in this case
+        filename = os.path.join(folder_location,link['href'].split('/')[-1])
+        with open(filename, 'wb') as f:
+            f.write(requests.get(urljoin(url,link['href'])).content)
